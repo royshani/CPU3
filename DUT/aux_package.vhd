3955232e147fd262422d56769129b8a33e1d2775
Yanaimsm <yanaimason94@gmail.com>
2025-05-13 11:59:22 +0300

    fixed ControlUNit aux_package

--- DUT/aux_package.vhd
+++ DUT/aux_package.vhd
@@ -21,23 +21,21 @@ package aux_package is
 --------------------------------------------------------
 -- IR component declaration
 --------------------------------------------------------
-component IR is
-    generic (Dwidth : integer := 16);
-    port (
-        clk_i         : in  std_logic;
-        ena_i         : in  std_logic;
-        rst_i         : in  std_logic;
-        RFaddr_rd_i   : in  std_logic_vector(1 downto 0);
-        RFaddr_wr_i   : in  std_logic_vector(1 downto 0);
-        IR_content_i  : in  std_logic_vector(Dwidth-1 downto 0);
-
-        opcode_o      : out std_logic_vector(3 downto 0);
-        addr_o        : out std_logic_vector(3 downto 0);
-        signext1_o    : out std_logic_vector(Dwidth-1 downto 0);
-        signext2_o    : out std_logic_vector(Dwidth-1 downto 0);
-        imm_to_PC_o   : out std_logic_vector(7 downto 0)
-    );
-end component;
+	component IR is
+		generic (Dwidth : integer := 16);
+		port (
+			clk_i         : in  std_logic;
+			ena_i         : in  std_logic;
+			rst_i         : in  std_logic;
+			RFaddr_rd_i   : in  std_logic_vector(1 downto 0);
+			RFaddr_wr_i   : in  std_logic_vector(1 downto 0);
+			IR_content_i  : in  std_logic_vector(Dwidth-1 downto 0);
+			opcode_o      : out std_logic_vector(3 downto 0);
+			signext1_o    : out std_logic_vector(Dwidth-1 downto 0);
+			signext2_o    : out std_logic_vector(Dwidth-1 downto 0);
+			imm_to_PC_o   : out std_logic_vector(7 downto 0)
+		);
+	end component;
 
 
 --------------------------------------------------------
@@ -69,37 +67,42 @@ end component;
 	component ControlUnit is
 		generic(StateLength : integer := 5);
 		port(
-			clk_i           : in  std_logic;
-			rst_i           : in  std_logic;
-			ena_i           : in  std_logic;
+			-- Clock, Reset, Enable
+			clk_i         : in std_logic;
+			rst_i         : in std_logic;
+			ena_i         : in std_logic;
 
-			ALU_c_i         : in  std_logic;
-			ALU_z_i         : in  std_logic;
-			ALU_n_i         : in  std_logic;
+			-- ALU status flags
+			ALU_c_i       : in std_logic;
+			ALU_z_i       : in std_logic;
+			ALU_n_i       : in std_logic;
 
-			opcode_i        : in  std_logic_vector(3 downto 0);
+			-- Instruction opcode
+			opcode_i      : in std_logic_vector(3 downto 0);
 
-			RF_out_o        : out std_logic;
-			Data_mem_out_o  : out std_logic;
-			Cout_o          : out std_logic;
-			Imm2_in_o       : out std_logic;
-			Imm1_in_o       : out std_logic;
-			IRin_o          : out std_logic;
-
-			RF_addr_o       : out std_logic_vector(1 downto 0);
-			PCsel_o         : out std_logic_vector(1 downto 0);
-			RF_WregEn_o     : out std_logic;
-			RF_rst_o        : out std_logic;
+			-- Datapath control signals
+			DTCM_wr_o       : out std_logic;
+			DTCM_addr_sel_o : out std_logic;
+			DTCM_addr_out_o : out std_logic;
+			DTCM_addr_in_o  : out std_logic;
+			DTCM_out_o      : out std_logic;
+			ALUFN_o         : out std_logic_vector(2 downto 0);
 			Ain_o           : out std_logic;
-			Cin_o           : out std_logic;
-			Mem_in_o        : out std_logic;
-			Data_MemEn_o    : out std_logic;
-			Pcin_o          : out std_logic;
+			RF_WregEn_o     : out std_logic;
+			RF_out_o        : out std_logic;
+			RF_addr_rd_o    : out std_logic_vector(1 downto 0);
+			RF_addr_wr_o    : out std_logic_vector(1 downto 0);
+			IRin_o          : out std_logic;
+			PCin_o          : out std_logic;
+			PCsel_o         : out std_logic_vector(1 downto 0);
+			Imm1_in_o       : out std_logic;
+			Imm2_in_o       : out std_logic;
 
-			ALU_op_o        : out std_logic_vector(2 downto 0);
-			status_bits_o   : out std_logic_vector(12 downto 0)
+			-- Debug/status output
+			status_bits_o   : out std_logic_vector(14 downto 0)
 		);
 	end component;
+
 --------------------------------------------------------
 -- ControlLines component declaration
 --------------------------------------------------------
